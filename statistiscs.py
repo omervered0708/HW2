2f98a4461ac4b226f9cb234f9460bd328ab2d850
omervered0708 <omer.vered@campus.technion.ac.il>
2021-04-24 13:14:11 +0300

    Initial commit

---
+++ statistiscs.py
@@ -0,0 +1,29 @@
+from math import ceil
+
+
+def mean(values):
+    """
+    return the mean of values
+    :param values: list of numeric values
+    :return: the mean of values
+    """
+    sum = 0
+    for value in values:
+        sum += value
+    return sum/len(values)
+
+
+def median(values):
+    """
+    return the median of values
+    :param values: list of numeric values
+    :return: median of values
+    """
+    values.sort()
+    n = len(values)
+    if n % 2 == 0:
+        med = values[(n//2)-1] + values[n//2]
+        med /= 2
+    else:
+        med = values[ceil(n/2)-1]
+    return float(med)
