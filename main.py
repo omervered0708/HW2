3bbaa5069a65837a9b2e5392c419650667ac67f3
omervered0708 <omer.vered@campus.technion.ac.il>
2021-04-26 16:19:44 +0300

    uploaded

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ def main(argv):
     dataset = data.Data(argv[1])
     dist = district.District(dataset)
     dist.filter_district({"L", "S"})
-    features =["hospitalized_with_symptoms", "Intensive_care", "total_hospitalized", "home_insulation"]
+    features = ["hospitalized_with_symptoms", "Intensive_care", "total_hospitalized", "home_insulation"]
     stat_funcs = [statistiscs.mean, statistiscs.median]
     dist.print_details(features, stat_funcs)
+
 
 if __name__ == '__main__':
     main(sys.argv)
